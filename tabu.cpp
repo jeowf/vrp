c1f67ca95a0733bea452125de23ed9a51f7302bf
Felipe Morais <moraisfelipe97@gmail.com>
2019-11-13 22:41:57 -0300

    tabu com c&w

--- tabu.cpp
+++ tabu.cpp
@@ -25,7 +25,25 @@ using namespace std;
 map<list<int> *, int> find_solution_tabu(graph & g){
 	map<list<int> *, int> res;
 
-	//TODO
+	auto cw = find_solution_CW(g);
+
+	vector<int> path;
+	path.push_back(1);
+
+	for (auto & e : cw){
+		if (!e.first->empty()){
+			for (auto & f : *e.first)
+				path.push_back(f);
+			path.push_back(1);
+
+		}
+	}
+
+	// Apaga isso dps \/
+	for (auto & e : path)
+		cout << e << " ";
+	cout << endl;
+	// apaga /\
 
 	return res;
 
