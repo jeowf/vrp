fed460edb7af226ec21e7d64d92ea6fb990d93d1
Felipe Morais <moraisfelipe97@gmail.com>
2019-11-17 16:25:21 -0300

    falta mutar

--- ga.cpp
+++ ga.cpp
@@ -25,8 +25,6 @@ auto aux_seed = seed;
 
 
 
-
-
 struct cromossome{
 	vector<int> sol;
 	float cost;
@@ -34,15 +32,18 @@ struct cromossome{
 	bool feasible;
 	bool mutation;
 
+	int i;
+	int e;
+
+	~cromossome(){}
+
 	cromossome(){ 
 		mutation = false;
 
-
-		sol.resize(100);
+		i = e = 0;
 	}
 
 	cromossome(vector<int> s, float c, float fi, bool f = true, bool m = false){
-		sol.resize(100);
 		
 		sol = s;
 		cost = c;
@@ -50,460 +51,529 @@ struct cromossome{
 		feasible = f;
 		mutation = m;
 
+		i = e = 0;
+
 	}
 
 	cromossome(const cromossome & c){
-		sol.resize(100);
 		sol = c.sol;
 		cost = c.cost;
 		fitness = c.fitness;
 		feasible = c.feasible;
 		mutation = c.mutation;
+		i = c.i;
+		e = c.e;
 	}
+
 };
 
-
-typedef vector<cromossome *> pop_t;
-pop_t population;
-
-pop_t sel;
-
-
-
-
-float fitness_function(float cost){
-	return 1.0/cost;
+bool comp(cromossome lhs, cromossome rhs){
+	return lhs.fitness > rhs.fitness;
 }
 
-void calcule_fitness(cromossome * c, graph & g){
 
-	float res = 0.0;
 
-	float dist = 0.0;	
+struct ga{
 
-	dist += g.matrix[1][c->sol[0]];
+	graph g;
 
-	for (int i = 0; i < c->sol.size()-1; i++){
-		dist+= g.matrix[c->sol[i]][c->sol[i+1]];
+	typedef vector<cromossome> pop_t;
+	pop_t population;
+
+	pop_t sel;
+
+	ga(graph & h){
+		g = h;
 	}
 
-	dist += g.matrix[c->sol[c->sol.size()-1]][1];
+	~ga(){ }
 
-	c->cost = dist;
-
-
-	int vehicle = 0;
-	vector<float> demands;
-
-	float local_demand = 0.0;
-
-	float penalty = 0.0;
-
-	for (int i = 0; i < c->sol.size(); i++){
-		//cout << local_demand << endl;
-		if (c->sol[i] == 1 or i == c->sol.size()-1){
-			//cout << local_demand << endl;
-
-			if (local_demand > g.capacity){
-				penalty += local_demand - g.capacity;
-				//cout << penalty << endl ;
-
-			}
-
-			demands.push_back(local_demand);
-			local_demand = 0;
-		} else {
-			local_demand += g.nodes[c->sol[i]].demand;
-		}
+	float fitness_function(float cost){
+		return 1.0/cost;
 	}
 
-	//c->demand = demands;
+	void calcule_fitness(cromossome & c){
 
-	if (penalty > 0.0)
-		c->feasible = false;
-	else
-		c->feasible = true;
+		float res = 0.0;
 
-	c->fitness = fitness_function(dist + penalty*epsilon);
-	c->mutation = false;
+		float dist = 0.0;	
 
-}
+		dist += g.matrix[1][c.sol[0]];
 
-void fitness(graph & g){
-	for (auto & e : population)
-		calcule_fitness(e, g);
-}
-
-
-
-void selection(graph & g){
-	sel.clear();
-
-	int n = population.size();
-
-
-	for (int i = 0; i < selection_size; i++){
-		sel.push_back(population[rand() % n]);
-		sel.push_back(population[rand() % n]);
-	}
-
-
-}
-
-vector<int> head_tail(const vector<int> & head, const vector<int> & tail, int cut_point){
-	
-	cout << "a.-1\n";
-
-	vector<int> res;
-	res.reserve(100);
-
-	int i = 0;
-
-	vector<int> ctrl(max(head.size(), tail.size()) + 1, 0);
-	vector<int> use_after;
-	use_after.reserve(100);
-
-	cout << "a.0\n";
-
-
-	while (i < cut_point){
-
-		int x = head[i];
-		res.push_back(x);
-
-		ctrl[head[i]] = -1; // proibido
-
-		if (ctrl[tail[i]] == 0){
-
-			ctrl[tail[i]] = 1; // permitido
-
-
-			if (tail[i] != 1)
-				use_after.push_back(tail[i]);
+		for (int i = 0; i < c.sol.size()-1; i++){
+			dist+= g.matrix[c.sol[i]][c.sol[i+1]];
 		}
 
+		dist += g.matrix[c.sol[c.sol.size()-1]][1];
 
-		i++;
-	}
-
-	ctrl[1] = 0;
-
-	cout << "a.1\n";
+		c.cost = dist;
 
 
-	while (i < tail.size()){
-		int v = 0;
+		int vehicle = 0;
+		//vector<float> demands;
 
-		if (ctrl[tail[i]] == -1){
-			for (int j = 0; j < use_after.size(); j++){
-				if (ctrl[use_after[j]] == 1){
-					v = use_after[j];
-					ctrl[use_after[j]] = -1;
-					break;
+		float local_demand = 0.0;
+
+		float penalty = 0.0;
+
+		int x_i = 0;
+		int x_e = 0;
+
+		int a_i = 0;
+		int a_e = 0;
+
+		for (int i = 0; i < c.sol.size(); i++){
+
+			if (c.sol[i] == 1 or i == c.sol.size()-1){
+
+				if (i == c.sol.size()-1){
+					a_e = i + 1;
+				} else {
+					a_e = i;
 				}
 
-			}
-		} else {
-			v = tail[i];
-		}
+				if (local_demand > g.capacity){
+					penalty += local_demand - g.capacity;
 
-		res.push_back(v);
-		i++;
-	}
+					x_i = a_i;
+					x_e = a_e;
 
-	cout << "a.2\n";
-
-
-	// caso falte algum valor nos nós
-	for (int j = 0; j < use_after.size(); j++){
-		if (ctrl[use_after[j]] == 1){
-			int v = use_after[j];
-			ctrl[v] = -1;
-			
-			for (int i = cut_point; i < res.size(); i++){
-				if (res[i] == 1){
-					res.insert(res.begin()+i, v);
-					//res[i] = v;
-					break;
 				}
 
+
+				local_demand = 0;
+
+				a_i = i+1;
+
+			} else {
+				local_demand += g.nodes[c.sol[i]].demand;
 			}
 		}
-	}
 
-	cout << "a.3\n";
+		//c.demand = demands;
 
-	// caso sobre algum valor
-
-	for (int i = 0; i < res.size(); ){
-		if (res[i] == 0){
-			res.erase(res.begin() + i);
+		if (penalty > 0.0){
+			c.feasible = false;
+			c.i = x_i;
+			c.e = x_e;
 		} else{
+			c.feasible = true;
+
+		}
+
+		c.fitness = fitness_function(dist + penalty*epsilon);
+		c.mutation = false;
+
+	}
+
+	void fitness(){
+		for (auto & e : population)
+			calcule_fitness(e);
+	}
+
+
+	void selection(){
+		sel.clear();
+
+		int n = population.size();
+
+		for (int i = 0; i < selection_size; i++){
+			cromossome a = population[rand() % n];
+			cromossome b = population[rand() % n];
+			sel.push_back(a);
+			sel.push_back(b);
+		}
+
+
+	}
+
+	void make_feasible(cromossome & c){
+
+		bool fe = c.feasible;
+/*
+		for (auto & e : c.sol)
+			cout << e << " ";
+		cout << endl;
+*/
+		while(!fe){
+			vector<int> ve;
+
+			int d =  c.e - c.i - 2;
+			int r = 0;
+
+			if (d > 0)
+				r = rand() & d;
+			//cout << "r :" << r << endl;
+
+			int i = 0;
+			for (; i <= c.i + r; i++){
+				ve.push_back(c.sol[i]);
+				//cout << c.sol[i] <<" ";
+			}
+			ve.push_back(1);
+			//cout << "1 ";
+			for(;i<c.sol.size(); i++){
+				ve.push_back(c.sol[i]);
+				//cout << c.sol[i] <<" ";
+
+			}
+			//cout << endl;
+
+			c.sol = ve;
+/*
+			for (auto & e : c.sol)
+				cout << e << " ";
+			cout << endl;
+*/
+			calcule_fitness(c);
+			fe = c.feasible;
+		}
+
+		//int x;
+		//cin >> x;
+	}
+
+
+	void clean_sol(cromossome & c){
+		vector<int> cl;
+		int last_e = 1;
+		for (int i = 0; i < c.sol.size(); i++){
+
+			if (c.sol[i] == 1 ){
+				if (last_e != 1){
+					last_e = c.sol[i];
+					cl.push_back(last_e);
+				}
+
+			} else {
+				last_e = c.sol[i];
+				cl.push_back(last_e);
+			}
+		}
+
+		if (cl.back() == 1)
+			cl.pop_back();
+
+		c.sol = cl;
+
+
+
+	}
+
+
+	void clean_pop(){
+		for (auto & e : population){
+			clean_sol(e);
+		}
+	}
+
+	vector<int> head_tail(vector<int> & head, vector<int> & tail, int cut_point){
+		
+
+		vector<int> res;
+		
+		map<int, bool> dont;
+
+		int i = 0;
+
+		while (i < cut_point){
+			res.push_back(head[i]);
+			dont[head[i]] = true;
 			i++;
 		}
-	}
-
-	cout << "a.4\n";
 
 
-
-	return res;
-}
-
-void one_point_crossover(cromossome * lhs, cromossome * rhs, graph & g){
-
-	cout << "a\n";
-	int n = min(lhs->sol.size(), rhs->sol.size());
-
-	int point = rand() % n;
-
-	vector<int> a;
-	a.reserve(100);
-	a = head_tail(rhs->sol, lhs->sol, point);
-
-	cout << "aaaa\n";
-	vector<int> b;
-	b.reserve(100);
-	b = head_tail(lhs->sol, rhs->sol, point);
-	cout << "b\n";
-
-
-	cromossome * c_a = new cromossome;
-
-	cout << "b.0\n";
-
-	cromossome * c_b = new cromossome;
-	cout << "b.1\n";
-	c_a->sol = a;
-
-/*
-	c_a->sol.resize(a.size());
-	for (int i = 0; i < a.size(); i++)
-		c_a->sol[i] = a[i];*/
-	
-	cout << "b.2\n";
-
-	c_b->sol = b;
-/*
-	c_b->sol.resize(b.size());
-	for (int i = 0; i < b.size(); i++)
-		c_b->sol[i] = b[i];
-	*/
-	cout << "b.3\n";
-
-
-	cout << "c\n";
-
-
-	calcule_fitness(c_a, g);
-	calcule_fitness(c_b, g);
-
-	cout << "d\n";
-
-
-	population.push_back(c_a);
-	population.push_back(c_b);
-
-	cout << "e\n";
-
-
-
-}
-
-
-
-void crossover(cromossome * lhs, cromossome * rhs, graph & g, int crossover_type = 1){
-
-	if (crossover_type == 1)
-		one_point_crossover(lhs, rhs, g);
-
-}
-
-
-
-void crossover(graph & g){
-
-
-	for (int i = 0; i < sel.size(); i += 2){
-		crossover(sel[i], sel[i+1], g);
-	}
-
-}
-
-
-void initialize_population(graph & g, int pop_size){
-
-
-	for (int i = 0; i < pop_size; ++i) {
-		cromossome * c = new cromossome;
-		vector<int> sol;
-
-		for (int j = 2; j < g.dimension; ++j) {
-			sol.push_back(j);
+		while (i < tail.size()){
+			res.push_back(tail[i]);
+			//dont[tail[i]] = true;
+			i++;
 		}
 
-		aux_seed = rand();
-		srand(aux_seed);
+		dont[1] = false;
+/*
+		for(int j = 2; j < g.dimension; j++){
+			cout << j << "(" << dont[j] << "),";
+		}
+		cout << endl;*/
+		//cout << "cut : " << cut_point << endl;
 
-		shuffle(sol.begin(), sol.end(), default_random_engine(aux_seed) );
+		int j = 0;
+		for (i = cut_point; i < res.size(); i++){
 
-		c->sol = sol;
-		population.push_back(c);
+			if (res[i] > 1 and dont[res[i]]){
+				//cout << res[i] << endl;
+				int v = 1;
+				for (; j < cut_point; j++){
 
-	}
-}
+					if ( tail[j] > 1 and !dont[tail[j]] ){
+						//cout << "subs " << tail[j] << endl;
+						v = tail[j];
+						dont[tail[j]] = true;
+						break;
+					}
+				}
 
-void make_feasible(cromossome * c, graph & g){
+				res[i] = v;
 
-	int n = c->sol.size();
-
-	float local_demand = 0.0;
-
-	//float penalty = 0.0;
-
-	int a = 0;
-	int b = n;
-
-	for (int i = 0; i < n; i++){
-		if (c->sol[i] == 1 or i == n-1){
-
-			b = i;
-			if (i == n-1)
-				b += 1;
-
-			if (local_demand > g.capacity){
-				//penalty += local_demand - g.capacity;
-				break;
-
+			} else {
+				dont[res[i]] = true;
 			}
 
-			a = i+1;
-			local_demand = 0;
-		} else {
-
-			local_demand += g.nodes[c->sol[i]].demand;
-		}
-	}
-
-	int r = rand() % (b-a-1);
-	c->sol.insert(c->sol.begin() + a + r, 1);
-
-}
-
-void mutation(cromossome * c, graph & g){
-
-	int n = c->sol.size();
-
-	int a = rand() % n;
-	int b = a;
-
-	while (b == a)
-		b = rand() % n;
-
-	int v_a = c->sol[a];
-	int v_b = c->sol[b];
-
-	c->sol[a] = v_b;
-	c->sol[b] = v_a;
-
-}
-
-void mutation(graph & g){
-
-	int n = population.size();
-	int q = n * mut_percent;
-
-	for(int i = 0; i < q; i++){
-		int c_i = rand() % n;
-		cromossome * c = population[c_i];
-
-		if (!c->mutation){
-			if (c->feasible)
-				mutation(c,g);
-			else
-				make_feasible(c,g);
-
-
-			c->mutation = true;
-			calcule_fitness(c,g);
 		}
 
-	}
+		vector<int> veacto;
+		dont[1] = true;
+		i = cut_point;
 
-}
+		for (; j < cut_point; j++){
+			
+			if ( tail[j] > 1 and !dont[tail[j]] ){
 
+				for (; i < res.size(); i++){
+					if (res[i] == 1){
 
-bool comp(cromossome * lhs, cromossome * rhs){
-	return lhs->fitness > rhs->fitness;
-}
+						res[i] = tail[j];
 
-cromossome genetic_algorithm(graph & g){
+						dont[tail[j]] = true;
+						break;
 
-	cout << "Using " << seed << " as seed\n";
-
-	srand(seed);
-	aux_seed = seed;
-
-	cromossome res;
-
-	initialize_population(g, population_size);
-	fitness(g);
-
-	int it_limit = 500;
-	int i = 0;
+					}
+				}
+				
+			}
+		}
 
 
 
-	while (i < 200){
+		///vector<int> missing;
 
-		
-		//crossover(population[rand() % population_size],population[rand() % population_size],g);
-		cout << "SEL\n";
-		sel.clear();
-		selection(g);
+		for (i = 2; i < g.dimension; i++){
+			if (!dont[i]){
+				for (int j = 0; j < res.size(); j++){
+					if (res[j] == 1){
+						res[j] = i;
+						dont[i] = true;
+					}
+				
+				}
+			}
+			
+		}
 
-		cout << "CROSS\n";
-		crossover(g);
-/*
-		cout << "MUT\n";
-		mutation(population, g);
 
-		cout << "SORT\n";
-		sort(population.begin(), population.end(), comp);
-		//population.resize(total_pop_size);
+		/*cout << "missing: ";
+		for (int i = 2; i < g.dimension; i++){
+			if (!dont[i]){
+				cout << i << " ";
+			}
+		}
+
+		cout << endl;
 */
-		cout << "END\n";
-		cout << i << endl;
-		i++;
+
+		return res;
 	}
 
-	for (auto & e: population[0]->sol)
-		cout << e << " ";
-	cout << endl;
+	void one_point_crossover(cromossome & lhs, cromossome & rhs){
 
-	cout << population[0]->cost << endl;
+		/*
+		cout << "lhs : ";
+
+		for (auto & e : lhs.sol)
+			cout << e << " ";
+		cout << "--" << lhs.sol.size() << endl;
+		
+
+		cout << "rhs : ";
+
+		for (auto & e : rhs.sol)
+			cout << e << " ";
+		cout << "--" << rhs.sol.size() << endl;
+		*/
+		int n = min(lhs.sol.size(), rhs.sol.size());
+
+		int point = rand() % n;
+
+		cromossome c_a;
+		vector<int> a = head_tail(rhs.sol, lhs.sol, point);
+		c_a.sol = a;
+
+		cromossome c_b;
+		vector<int> b = head_tail(lhs.sol, rhs.sol, point);
+		c_b.sol = b;
+
+
+		calcule_fitness(c_a);
+		calcule_fitness(c_b);
+
+		/*
+		cout << "c_a : ";
+
+		for (auto & e : c_a.sol)
+			cout << e << " ";
+		cout << "--" << c_a.sol.size() << endl;
+		
+
+		cout << "c_b : ";
+
+		for (auto & e : c_b.sol)
+			cout << e << " ";
+		cout << "--" << c_b.sol.size() << endl;
+		*/
+		make_feasible(c_a);
+		make_feasible(c_b);
+/*
+		cout << "c_a : ";
+
+		for (auto & e : c_a.sol)
+			cout << e << " ";
+		cout << "--" << c_a.sol.size() << endl;
+		
+
+		cout << "c_b : ";
+
+		for (auto & e : c_b.sol)
+			cout << e << " ";
+		cout << "--" << c_b.sol.size() << endl;
+*/
+		population.push_back(c_a);
+		population.push_back(c_b);
+
+	}
+
+
+	void crossover(){
+
+		int x = 0;
+		for (int i = 0; i < sel.size(); i += 2){
+			one_point_crossover(sel[i], sel[i+1]);
+			//cin >> x;
+		}
+	}
+
+
+	void initialize_population(int pop_size){
+
+		for (int i = 0; i < pop_size; ++i) {
+			cromossome c;
+			vector<int> sol;
+
+			for (int j = 2; j < g.dimension; ++j) {
+				sol.push_back(j);
+			}
+
+			aux_seed = rand();
+			srand(aux_seed);
+
+			shuffle(sol.begin(), sol.end(), default_random_engine(aux_seed) );
+
+			c.sol = sol;
+
+
+			calcule_fitness(c);
+
+			make_feasible(c);
+
+			population.push_back(c);
+
+		}
+	}
+
 	
-	int k;
-	cin >> k;
 
-	//cout << endl << endl;
-	//population.clear();
-	return res;
+	void execute(){
 
-}
+		cout << "Using " << seed << " as seed\n";
+
+		srand(seed);
+		aux_seed = seed;
+
+		cromossome res;
+
+		initialize_population(population_size);
+		//fitness();
+
+		int it_limit = 500;
+		int i = 0;
+		
+
+		// for (auto & v : population){
+		// 	for (auto & e : v.sol){
+		// 		cout << e << " ";
+		// 	}
+		// 	cout << endl;
+		// }
+
+		/*
+
+		vector<cromossome> sel;
+
+		vector<int> a = { 2, 3, 1, 5, 6, 1, 4, 7};
+		vector<int> b = { 7, 1, 2, 1, 5, 6, 1, 3, 4 };
+		cromossome c_a;
+		cromossome c_b;
+
+		c_a.sol = a;
+		c_b.sol = b;
+
+		one_point_crossover(c_a,c_b);
+		//mutation(population, g);
+*/
+		
+
+
+		while (i < 200){
+
+			
+			//crossover(population[rand() % population_size],population[rand() % population_size],g);
+			//cout << "SEL\n";
+			selection();
+
+			//cout << "CROSS\n";
+			crossover();
+
+			clean_pop();
+
+			sort(population.begin(), population.end(), comp);
+			population.resize(total_pop_size);
+
+
+			i++;
+		}
 
 
 
+		for (auto & e: population[0].sol)
+			cout << e << " ";
+		cout << endl;
+
+		cout << population[0].cost << endl;
+
+		if (!population[0].feasible){
+			cout << population[0].i << ", " << population[0].e << endl;
+		}
+		
+		//int k;
+		//cin >> k;
+
+		//cout << endl << endl;
+		//population.clear();
+	}
+
+
+
+};
 
 
 
 map<list<int> *, int> find_solution_GA(graph & g){
 	map<list<int> *, int> res;
 
-	auto c = genetic_algorithm(g);
+	ga gen(g);
+	gen.execute();
 
-
-	population.clear();
+	int a;
+	//cin >> a;
 
 	return res;
 
